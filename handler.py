bfd632c24b7f4a7707877b85ff1d83364d44f9d1
Sebastian Erhardt <sebastian@ex-studios.net>
2019-02-27 22:56:11 +0100

    files

---
+++ handler.py
@@ -0,0 +1,22 @@
+import json
+from botocore.vendored import requests
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    print(event)
+
+    r = requests.post('https://prostate.cdtm.ex-studios.net/api/1/iot/medication', json=event)
+
+    response = {
+        "statusCode": 200,
+        "req": json.dumps(body),
+        "res": json.dumps(r)
+    }
+
+    return response
+
